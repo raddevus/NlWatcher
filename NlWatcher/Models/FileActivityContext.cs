b40acac49294ca1992c732b4374352f0e433c422
raddevus <1radeutsch1@gmail.com>
2026-01-12 09:37:59 -0500

    context class set as dbcontext type

--- NlWatcher/Models/FileActivityContext.cs
+++ NlWatcher/Models/FileActivityContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace NlWatcher.Models;
 
-public class FileActivityContext{
+public class FileActivityContext : DbContext{
 }
